eb1b0bc481dd6959de8740ceeb39d76c8df97582
Paul Bienkowski <opatutlol@aol.com>
2012-09-18 22:36:48 +0200

    Added delegate to test

--- d-signals/test.d
+++ d-signals/test.d
@@ -2,6 +2,18 @@ import std.stdio;
 
 import signals;
 
+class Test {
+    string name;
+
+    this(string name) {
+        this.name = name;
+    }
+
+    void slotDelegate(int x, string y) {
+        writefln("Delegate of test object %s: %s %s", this.name, x, y);
+    }
+}
+
 void slot1(int i, string s) {
     writefln("SLOT 1: %s\t%s", i, s);
 }
@@ -17,6 +29,12 @@ void main() {
     uint s2 = signal.connect(&slot2);
 
     signal.emit(10, "rofl");
+
+    Test t1 = new Test("TestObject1");
+    Test t2 = new Test("TestObject2");
+
+    signal.connect(&t1.slotDelegate);
+    signal.connect(&t2.slotDelegate);
 
     signal.disconnect(s2);
     signal.emit(20, "roflmao");
